2e510014e2da3010edb3412ababc1361b66b7171
Jiri Brusenbauch <jiri.brusenbauch@gmail.com>
2012-11-24 12:36:10 +0100

    splash

--- gen/cz/mapakamer/BuildConfig.java
+++ gen/cz/mapakamer/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.mapakamer;
+package cz.mapakamer;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- gen/cz/mapakamer/R.java
+++ gen/cz/mapakamer/R.java
@@ -5,16 +5,31 @@
  * should not be modified by hand.
  */
 
-package com.example.mapakamer;
+package cz.mapakamer;
 
 public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int camera_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int logo_iure=0x7f020002;
+    }
+    public static final class id {
+        public static final int devModeInfo=0x7f060001;
+        public static final int imageView1=0x7f060000;
+        public static final int progressBar1=0x7f060004;
+        public static final int textView1=0x7f060002;
+        public static final int textView2=0x7f060003;
+    }
+    public static final class layout {
+        public static final int activity_login=0x7f030000;
+        public static final int activity_splash=0x7f030001;
     }
     public static final class string {
-        public static final int app_name=0x7f030000;
+        public static final int app_name=0x7f040000;
+        public static final int logo=0x7f040002;
+        public static final int version=0x7f040001;
     }
     public static final class style {
         /** 
@@ -38,10 +53,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
