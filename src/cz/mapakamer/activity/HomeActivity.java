99d6cc243d26562cda22188060e603ec2988d6a8
Jiri Brusenbauch <jiri.brusenbauch@gmail.com>
2012-11-24 12:51:31 +0100

    ok

---
+++ src/cz/mapakamer/activity/HomeActivity.java
@@ -0,0 +1,22 @@
+package cz.mapakamer.activity;
+
+import cz.mapakamer.R;
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class HomeActivity extends Activity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        
+       
+    }
+	
+}
